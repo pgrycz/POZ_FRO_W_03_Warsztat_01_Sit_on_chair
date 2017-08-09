388e0fbb6387191b516641906459119468c2e2c3
Pawel <pawelgrycz@gmail.com>
2017-08-09 19:42:45 +0200

    test pushing branch

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    // sync = require('browser-sync'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+// Ciało naszego zadania.
+// console.log('hallo!');
+  return gulp.src('scss/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(plumber())
+  // .pipe(sync())
+  .pipe(sass({
+    outputStyle: 'expanded',
+    sourceComments: 'map',
+    errLogToConsole: true}))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*.scss', ['sass'])
+})
