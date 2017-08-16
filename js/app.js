1bbbceb60a2d3987537238d309967bf4f65b904f
Pawel <pawelgrycz@gmail.com>
2017-08-16 05:58:31 +0200

    minor changes, added menu

--- js/app.js
+++ js/app.js
@@ -1,40 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  let menu = document.getElementsByTagName('nav'),
-      menuLinks = menu[0].children,
-      triangle = document.createElement('div'),
-      subMenu = document.createElement('div'),
-      subLink1 = document.createElement('a'),
-      subLink2 = document.createElement('a'),
-      subLink3 = document.createElement('a');
-  subMenu.classList.add('submenu');
-  triangle.classList.add('triangle');
-  subMenu.style.display = 'none';
-  subLink1.innerText = 'Aktualności';
-  subLink2.innerText = 'Nasz team';
-  subLink3.innerText = 'Historia';
-  subLink1.setAttribute('href', '#');
-  subLink2.setAttribute('href', '#');
-  subLink3.setAttribute('href', '#');
-  subMenu.appendChild(triangle);
-  subMenu.appendChild(subLink1);
-  subMenu.appendChild(subLink2);
-  subMenu.appendChild(subLink3);
-  menuLinks[0].appendChild(subMenu);
+  let menu = document.getElementsByClassName('header__nav'),
+      menuLink = menu[0].getElementsByTagName('li'),
+      subMenu = document.getElementsByClassName('header__submenu');
 
-
-  function showSubMenu() {
-    subMenu.style.display = 'block';
-  };
-
-  function hideSubMenu() {
-    subMenu.style.display = 'block';
-    setTimeout(function() {
-      subMenu.style.display = 'none';
-    }, 500);
-  };
-
-  menuLinks[0].addEventListener('mouseover', showSubMenu);
-  menuLinks[0].addEventListener('mouseout', hideSubMenu);
+  menuLink[0].addEventListener('click', function() {
+    if (subMenu[0].style.display === 'none') {
+      subMenu[0].style.display = 'block';
+    } else {
+      subMenu[0].style.display = 'none';
+    }
+  });
 
 });
