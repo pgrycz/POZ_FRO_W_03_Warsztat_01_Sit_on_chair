948a5111f7478857bf3e5c40e555abdec339802c
pawel <pawelgrycz@gmail.com>
2018-02-24 15:49:29 +0100

    added jquery features

--- js/app.js
+++ js/app.js
@@ -1,33 +1,6 @@
-// document.addEventListener('DOMContentLoaded', function() {
-
-//   // on hover menu header
-//   let menu = document.getElementsByClassName('header__nav');
-//   let menuLinks = menu[0].children;
-//   let menuBridge = document.getElementsByClassName('header__bridge');
-
-//   function showSubMenu() {
-//     this.children[0].nextElementSibling.style.display = 'block';
-//   }
-//   function hideSubMenu() {
-//     this.style.display = 'none';
-//   }
-
-//   for (let i = 0; i < menuLinks.length; i ++) {
-//     menuLinks[i].addEventListener('mouseover', showSubMenu);
-//   }
-//   for (let i = 0; i < menuBridge.length; i ++) {
-//     menuBridge[i].addEventListener('mouseout', hideSubMenu);
-//   }
-
-// });
-
-$(function($) {
+$(function() {
 
   // on hover menu header
-  let menu = $(document).find('.header__nav');
-  let menuLinks = menu.children();
-  let menuBridge = $(document).find('.header__bridge');
-  
   function showSubMenu() {
     $(this).children().fadeIn(300);
   }
@@ -35,13 +8,13 @@ $(function($) {
     $(this).fadeOut(300);
   }
  
-  menuLinks.on('mouseenter', showSubMenu);
-  menuBridge.on('mouseleave', hideSubMenu);
+  $('.header__nav').children().on('mouseenter', showSubMenu);
+  $('.header__bridge').on('mouseleave', hideSubMenu);
 
   // slider on click
   let slideCount = $('.slider__imgwrap ul li').length;
-  let slideWidth = $('.slider__imgwrap ul li img').width();
-  let slideHeight = $('.slider__imgwrap ul li img').height();
+  let slideWidth = $('.slider__imgwrap ul li').width();
+  let slideHeight = $('.slider__imgwrap ul li').height();
   let slideUlWidth = slideCount * slideWidth;
 
   $('slider__imgwrap').css({width: slideWidth, height: slideHeight});
@@ -50,18 +23,18 @@ $(function($) {
 
   function prevSlide() {
     $('.slider__imgwrap ul').animate({
-      'left': + slideWidth
+      left: slideWidth
     }, 600, function () {
       $('.slider__imgwrap ul li:last-child').prependTo('.slider__imgwrap ul');
-      $('.slider__imgwrap ul').css('left', '');
+      $('.slider__imgwrap ul').css({left: ''});
     });
   }
   function nextSlide() {
     $('.slider__imgwrap ul').animate({
-      'left': - slideWidth
+      right: slideWidth
     }, 600, function () {
       $('.slider__imgwrap ul li:first-child').appendTo('.slider__imgwrap ul');
-      $('.slider__imgwrap ul').css('left', '');
+      $('.slider__imgwrap ul').css({right: ''});
     });
   }
 
@@ -69,8 +42,6 @@ $(function($) {
   $('.slider__next').on('click', nextSlide);
 
   // description fade elements on hover
-  let descriptionBox = $(document).find('.description__img');
-
   function fadeOut() {
     $(this).children().eq(1).delay(100).fadeOut(200);
     $(this).children().eq(2).delay(100).fadeOut(200);
@@ -80,7 +51,57 @@ $(function($) {
     $(this).children().eq(2).delay(100).fadeIn(200);
   }
 
-  descriptionBox.on('mouseenter', fadeOut);
-  descriptionBox.on('mouseleave', fadeIn);
+  $('.description__img').on('mouseenter', fadeOut);
+  $('.description__img').on('mouseleave', fadeIn);
+
+  // order calculate item price
+  let rows = $('.order__calculate').children().children().children();
+  let value = 0;
+  let sum = 0;
+
+  function typeChange() {
+    $('#chairType').each(function() {
+      value = Number($('#chairType option:selected').attr('data-price'));
+      rows.children().eq(1).text(value);
+      price();
+    });
+  }
+  function colorChange() {
+    $('#chairColor').each(function() {
+      value = Number($('#chairColor option:selected').attr('data-price'));
+      rows.children().eq(3).text(value);
+      price();
+    });
+  }
+  function materialChange() {
+    $('#chairMaterial').each(function() {
+      value = Number($('#chairMaterial option:selected').attr('data-price'));
+      rows.children().eq(5).text(value);
+      price();
+    });
+  }
+  function transportPick() {
+    value = 100;
+    if ($('#transport').is(':checked')) {
+      rows.children().eq(7).text(value);
+      sum += value;
+    } else {
+      rows.children().eq(7).text(0);
+      sum -= value;
+    }
+    price();
+  }
+  function price() {
+    sum =  Number(rows.children().eq(1).text()) 
+         + Number(rows.children().eq(3).text()) 
+         + Number(rows.children().eq(5).text())
+         + Number(rows.children().eq(7).text());
+    rows.children().eq(9).text(sum);
+  }
+
+  $('#chairType').on('change', typeChange);
+  $('#chairColor').on('change', colorChange);
+  $('#chairMaterial').on('change', materialChange);
+  $('#transport').on('click', transportPick);
 
 });
